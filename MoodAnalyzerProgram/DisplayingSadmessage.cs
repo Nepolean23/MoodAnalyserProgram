7f9ad5e1eb7102018f7580b0547cab8bcdd41a18
Nepolean23 <nlean1996@gmail.com>
2023-01-30 12:33:09 +0530

    [NEPOLEAN]-Code for Displaying of SadMessage

---
+++ MoodAnalyzerProgram/DisplayingSadmessage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProgram
+{
+    public class DisplayingSadmessage
+    {
+            public string message;
+            /// <summary>
+            /// Default Constructor
+            /// </summary>
+            public DisplayingSadmessage()
+            {
+                
+
+            }
+            /// <summary>
+            /// Parameterised Constructor
+            /// </summary>
+            public DisplayingSadmessage(string message)
+            {
+                this.message = message.ToUpper();
+            }
+            public string AnalyseMoodWithoutConstructor(string message)
+            {
+                if (message.ToLower().Contains("sad"))
+                    return "SAD";
+
+                else return "HAPPY";
+
+            }
+            //UC-2 Using Try Catch Blocks to Handle Null Exception.
+            public string AnalyseMood()
+            {
+                try
+                {
+                    if (message.ToLower().Contains("sad"))
+                        return "SAD";
+                    else return "HAPPY";
+                }
+                catch (NullReferenceException)
+                {
+                    return "HAPPY";
+                }
+
+            }
+    }
+}
---
+++ MoodAnalyzerProgram/SimpleMoodAnalyzerProgram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProgram
+{
+    public class SimpleMoodAnalyzerProgram
+    {
+        public string message;
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public SimpleMoodAnalyzerProgram()
+        {
+
+        }
+        /// <summary>
+        /// Parameterised Constructor
+        /// </summary>
+        public SimpleMoodAnalyzerProgram(string message)
+        {
+            this.message = message.ToUpper();
+        }
+        public string AnalyseMoodWithoutConstructor(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+                return "SAD";
+
+            else return "HAPPY";
+
+        }
+        public string AnalyseMood()
+        {
+            if (message.ToLower().Contains("sad"))
+                return "SAD";
+
+            else return "HAPPY";
+        }
+
+    }
+}
