12ba04c7bd5f8dd6633c5a65ee27c83c3dea0961
Nepolean23 <nlean1996@gmail.com>
2023-01-31 15:23:33 +0530

    [NEPOLEAN]-Code for CustomExpection MoodAnalysis

---
+++ MoodAnalyzerProgram/CustomMoodAnalysisException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProgram
+{
+    public class CustomMoodAnalysisException : Exception
+    {
+        public enum ExceptionType
+        {
+            EMPTY_MOOD,
+            NULL_MOOD,
+        }
+
+        public ExceptionType exceptionType;
+        public CustomMoodAnalysisException(ExceptionType exceptionType, string message) : base(message)
+        {
+            this.exceptionType = exceptionType;
+        }
+    }
+
+}
---
+++ MoodAnalyzerProgram/MoodAnalysisException.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProgram
+{
+    public class MoodAnalysisException
+    {
+        public string message;
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public MoodAnalysisException()
+        {
+
+        }
+        /// <summary>
+        /// Parameterised Constructor
+        /// </summary>
+        public MoodAnalysisException(string message)
+        {
+            this.message = message.ToUpper();
+        }
+        public string AnalyseMoodWithoutConstructor(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+                return "SAD";
+
+            else return "HAPPY";
+
+        }
+        //UC-2 Using Try Catch Blocks to Handle Null Exception.
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (message.ToLower().Contains("sad"))
+                    return "SAD";
+                else return "HAPPY";
+            }
+            catch (NullReferenceException)
+            {
+                return "HAPPY";
+            }
+
+        }
+
+
+
+
+
+    }
+
+}
+
